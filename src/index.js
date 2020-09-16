768ca968aed1a4f5593a11ab51ec1788aaf6ad9d
Danish <danishriz100@gmail.comgit commitexit>
2020-09-16 15:05:55 -0600

    Basic Nav bar added

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-import { createStore } from 'redux';
-import {Provider} from 'react-redux';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route } from "react-router-dom";
 
-import Calculator from './components/Calculator'
-import OldResultsList from './components/OldResultsList'
+import Calculator from "./components/Calculator";
+import OldResultsList from "./components/OldResultsList";
+import Nav from "./components/Nav";
 
-import resultReducer from './reducers/resultList';
+import resultReducer from "./reducers/resultList";
 
-
-const store = createStore (resultReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-
+const store = createStore(
+  resultReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
-
-    <Calculator/>
-   
-    <OldResultsList/>
- 
-
-   
+    <Router>
+      <Nav />
+      <Route path="/" component={Calculator} />
+      <Route path="/list" component={OldResultsList} />
+    </Router>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
--- src/reducers/resultList.js
+++ src/reducers/resultList.js
@@ -1,19 +1,16 @@
-const resultReducer = (state = [] , action) => {
+const resultReducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_NEW_RESULT":
+      const newItem = {
+        task: action.payload,
+      };
+      const newResultList = [...state];
+      newResultList.push(newItem);
+      return newResultList;
 
-    switch(action.type)
-    {
-        case 'ADD_NEW_RESULT':
-            const newItem = {
-                task: action.payload
-            };
-            const newResultList=[...state];
-            newResultList.push(newItem)
-            return newResultList;
-
-            default :
-            return state
-
-    }//end switch
-}//end result reducer
+    default:
+      return state;
+  } //end switch
+}; //end result reducer
 
 export default resultReducer;
